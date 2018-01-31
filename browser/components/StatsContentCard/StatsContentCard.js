f962584bad0268252c88dae6b06953ed0414053b
xifengjin88 <xifeng.jin88@gmail.com>
2018-01-30 19:45:17 -0500

    created  statsContentCard component and created Util folders for some common utility components for the project

---
+++ browser/components/StatsContentCard/StatsContentCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Aux }from '../../Util';
+import './StatsContentCard.scss';
+
+console.log(Aux)
+
+const statsContentCard = (props) => (
+  <Aux>
+    <h1>This is stats content card</h1>
+  </Aux>
+)
+
+export default statsContentCard;
