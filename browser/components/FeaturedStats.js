d53ac9646482299d18f3ecbd45fbef029005f828
Wale Jegs <wale.a.jegede@gmail.com>
2018-02-09 21:23:21 -0500

    cleaned code

--- browser/components/FeaturedStats.js
+++ browser/components/FeaturedStats.js
@@ -7,48 +7,59 @@ import './FeaturedStats.scss'
 class FeaturedStats extends Component {
 
   componentDidMount(){
-    this.props.wins()
-    this.props.kills()
-    this.props.plays()
+  this.props.wins()
+  this.props.kills()
+  this.props.plays()
   }
 
   render(){
-    const { wins, plays, kills} = this.props.stats;
-    console.log(wins, plays, kills)
-    return (
-      <div className="featured-stat">
-        {
-          wins && <StatsContentCard imageUrl={wins.picture} godName={wins.name} label="Most Wins" stat={wins.wins} />
-        }
-        {
-          kills && <StatsContentCard imageUrl={kills.picture} godName={kills.name} label="Most Kills" stat={kills.kills} />
-        }
-        {
-          plays && <StatsContentCard imageUrl={plays.picture} godName={plays.name} label= "Most Game Played" stat={plays.gamesPlayed} />
-        }
+  const { wins, plays, kills} = this.props.stats;
+  return (
+    <div className="featured-stat">
+    {
+      wins && <StatsContentCard
+          imageUrl={wins.picture}
+          godName={wins.name}
+          label="Most Wins"
+          stat={wins.wins} />
+    }
+    {
+      kills && <StatsContentCard
+            imageUrl={kills.picture}
+            godName={kills.name}
+            label="Most Kills"
+            stat={kills.kills} />
+    }
+    {
+      plays && <StatsContentCard
+            imageUrl={plays.picture}
+            godName={plays.name}
+            label= "Most Game Played"
+            stat={plays.gamesPlayed} />
+    }
 
-      </div>
-    )
+    </div>
+  )
   }
 }
 
 const mapState = (state) => {
   return {
-    stats: state.stats
+  stats: state.stats
   }
 }
 
 const mapDispatch = dispatch => {
   return {
-      wins() {
-        dispatch(getMostWinsThunk());
-      },
-      kills() {
-        dispatch(getMostKillsThunk());
-      },
-      plays() {
-        dispatch(getMostPlaysThunk());
-      }
+    wins() {
+    dispatch(getMostWinsThunk());
+    },
+    kills() {
+    dispatch(getMostKillsThunk());
+    },
+    plays() {
+    dispatch(getMostPlaysThunk());
+    }
   }
 }
 
